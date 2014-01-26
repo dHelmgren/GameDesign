ccb6834a7cce4eb59416912da06905850ed0f324
Sam Golloway <sgolloway14@gmail.com>
2014-01-26 13:03:13 -0800

    everything is better when you push to master

---
+++ Library/Assets/Rotation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	 public float PlanetRotateSpeed = -25.0f;
+	 public float speed = 10.0f;
+	int move = 0;
+	public bool movement = false;
+	public bool clockWise = false;
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) == true ) 
+		{
+			clockWise = !clockWise;	
+		}
+
+		else if (Input.GetKeyDown(KeyCode.A) == true ) 
+		{
+			movement = !movement;
+			
+		}
+		else if(Input.GetKeyDown(KeyCode.S) == true)
+		{
+			movement = !movement;
+			clockWise = !clockWise;
+		}
+	}
+	void FixedUpdate() 
+	{
+	
+
+   			 if (movement) 
+			 {
+				 if (clockWise) 
+				 {
+						transform.RotateAround (GameObject.Find ("ball2").transform.position, (-1) * Vector3.up, speed * Time.deltaTime);
+				 } 
+			     else 
+			     {
+						transform.RotateAround (GameObject.Find ("ball2").transform.position, Vector3.up, speed * Time.deltaTime);
+				 }
+			}
+    }
+}
