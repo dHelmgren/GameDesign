b84357dbe61eba1fdb683b959454b56434d2238b
unknown <helmgren14@EN205-15827.campus.up.edu>
2014-02-06 12:08:57 -0800

    Scoring implemented with timer

---
+++ Library/Assets/Assets - Copy/GameController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public GameObject pickups;
+	public Vector3 spawnValuesPick;
+	public static float timer;
+	public static bool timeStarted = false;
+	//This is the offset for the score. How many points
+	//per second
+	public int scoreOffSet;
+
+	void Start()
+	{
+		//Start the timer
+		timeStarted = true;
+		StartCoroutine (SpawnWaves ());
+	}
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.R)) {
+			//reset the score
+			timeStarted = false;
+			timer = 0;
+			Application.LoadLevel(Application.loadedLevel);
+
+				}
+		if (timeStarted == true) {
+			timer = timer + Time.deltaTime;
+				}
+	}
+
+	void OnGUI(){
+		//This code below is if you want to display the time in the game
+			//int minutes = Mathf.FloorToInt (timer / 60F);
+			//int seconds = Mathf.FloorToInt (timer - minutes * 60);
+
+			//string niceTime = string.Format ("{0:00}:{1:00}", minutes, seconds);
+			//GUI.Label (new Rect (10, 10, 250, 100), niceTime);
+
+		//This code is if you want to display the score for the round
+		int score = Mathf.RoundToInt(timer * scoreOffSet);
+		string displayScore = string.Format ("{0:0}:{1: 000000}", "Score", score);
+
+		GUI.Label (new Rect (10, 10, 250, 100), displayScore);
+
+		}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(spawnWait);
+		while(true)
+		{
+			for (int i = 0;i< hazardCount;i++) 
+			{	
+				Vector3 spawnPosition = new Vector3 (spawnValues.x, spawnValues.y, Random.Range (-spawnValues.z, spawnValues.z));
+				Quaternion spawnRotation = new Quaternion ();
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+				if(i%2 == 0)
+				{
+
+					Vector3 spawnPositionPick = new Vector3 (spawnValuesPick.x, spawnValuesPick.y, Random.Range (-spawnValuesPick.z, spawnValuesPick.z));
+					Quaternion spawnRotationPick = new Quaternion ();
+					Instantiate (pickups, spawnPositionPick, spawnRotation);
+				}
+				yield return new WaitForSeconds(spawnWait);
+	    	}
+		}
+	}
+}
---
+++ Library/Assets/Assets - Copy/Rotations.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotations : MonoBehaviour {
+
+	 public float PlanetRotateSpeed = -25.0f;
+	 public float speed = 10.0f;
+	int move = 0;
+	public bool movement = false;
+	public bool clockWise = false;
+	public bool washit = false;
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) == true ) 
+		{
+			clockWise = !clockWise;
+			washit = false;
+			rigidbody.velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+		}
+
+		else if (Input.GetKeyDown(KeyCode.A) == true ) 
+		{
+			movement = !movement;
+			washit = false;
+			rigidbody.velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+			
+		}
+		else if(Input.GetKeyDown(KeyCode.S) == true)
+		{
+			movement = !movement;
+			clockWise = !clockWise;
+			washit = false;
+			rigidbody.velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+		}
+	}
+	void FixedUpdate() 
+	{
+	
+		if (!washit) {
+						if (movement) {
+								if (clockWise) {
+										transform.RotateAround (GameObject.Find ("ball2").transform.position, (-1) * Vector3.up, speed * Time.deltaTime);
+								} else {
+										transform.RotateAround (GameObject.Find ("ball2").transform.position, Vector3.up, speed * Time.deltaTime);
+								}
+						} else {
+
+								if (clockWise) {
+										transform.RotateAround (GameObject.Find ("Ball1").transform.position, (-1) * Vector3.up, speed * Time.deltaTime);
+								} else {
+										transform.RotateAround (GameObject.Find ("Ball1").transform.position, Vector3.up, speed * Time.deltaTime);
+								}
+
+
+						}
+				}
+
+    }
+}
