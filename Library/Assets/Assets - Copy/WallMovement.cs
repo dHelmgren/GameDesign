dd773e73ec29f38f29dbdee6f3fce2115356077c
unknown <helmgren14@EN205-15827.campus.up.edu>
2014-02-06 12:23:27 -0800

    Pickups should have rotation

--- Library/Assets/Assets - Copy/WallMovement.cs
+++ Library/Assets/Assets - Copy/WallMovement.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class WallMovement : MonoBehaviour {
 	public float wallSpeed = -6.0f;
+	public float tumble;
 	// Use this for initialization
 	void Start () {
 	
+		//give the pick up a rotation
+		if (gameObject.tag == "pickUp") 
+		{
+			rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+		}
 	}
 	
 	// Update is called once per frame
